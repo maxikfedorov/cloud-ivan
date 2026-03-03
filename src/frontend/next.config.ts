c8b95a647d67a86bd508feba648418d014868528
maxikfedorov <xedzer2015@gmail.com>
2026-03-03 21:35:09 +0300

    1

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone", // <--- ФУНДАМЕНТАЛЬНО ВАЖНО ДЛЯ DOCKER
   
-  // Если у тебя тут были какие-то другие настройки (например images), оставь их
-  eslint: {
-    ignoreDuringBuilds: true, // Рекомендую добавить, чтобы eslint не ронял сборку в докере
-  },
 };
 
 export default nextConfig;
